e555b6eea753cda181697390ce9a0144a4643411
4bjith <abhijithpops1890@gmail.com>
2025-10-19 09:42:30 +0530

    added route for updatedriver

--- Routes/Driver.js
+++ Routes/Driver.js
@@ -1,11 +1,11 @@
-import express from "express"
-import { SignIn, SignUp } from "../Controller/Driver.js";
-
+import express from "express";
+import { SignIn, SignUp, updateDriver } from "../Controller/Driver.js";
 
 const router = express.Router();
 
 // routes for drivers
-router.post("/driverlogin",SignIn);
-router.post("/signup-driver",SignUp)
+router.post("/driverlogin", SignIn);
+router.post("/signup-driver", SignUp);
+router.put("/driver/update", updateDriver);
 
 export default router;
